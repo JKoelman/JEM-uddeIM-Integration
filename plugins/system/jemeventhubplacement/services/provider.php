14615d9ee26f7c966ae98911a1d9d9a087a64420
Jaap <JKoelman@users.noreply.github.com>
2026-08-18 20:51:06 +0200

    Add Event Hub placement plugin provider

---
+++ plugins/system/jemeventhubplacement/services/provider.php
@@ -0,0 +1,27 @@
+<?php
+defined('_JEXEC') or die;
+
+use Joomla\CMS\Extension\PluginInterface;
+use Joomla\CMS\Factory;
+use Joomla\CMS\Plugin\PluginHelper;
+use Joomla\DI\Container;
+use Joomla\DI\ServiceProviderInterface;
+use Joomla\Event\DispatcherInterface;
+use JemEventHub\Plugin\System\JemEventHubPlacement\Extension\JemEventHubPlacement;
+
+return new class implements ServiceProviderInterface {
+    public function register(Container $container): void
+    {
+        $container->set(
+            PluginInterface::class,
+            function (Container $container) {
+                $plugin = new JemEventHubPlacement(
+                    $container->get(DispatcherInterface::class),
+                    (array) PluginHelper::getPlugin('system', 'jemeventhubplacement')
+                );
+                $plugin->setApplication(Factory::getApplication());
+                return $plugin;
+            }
+        );
+    }
+};
